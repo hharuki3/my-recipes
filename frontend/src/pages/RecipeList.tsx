9ed6264de9a573c5d9abaacf7dc3a9f67d265d75
hharuki3 <pingyanchunshu@gmail.com>
2026-06-27 21:59:24 +0900

    feat: レシピ一覧ページにAPIデータ取得と表示を実装

--- frontend/src/pages/RecipeList.tsx
+++ frontend/src/pages/RecipeList.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react"
+
+type Recipe = {
+    id: number
+    name: string
+    cooked_count: number
+}
+
 function RecipeList() {
-    return <h1>レシピ一覧</h1>
+    const [recipes, setRecipes] = useState<Recipe[]>([])
+
+    useEffect(() => {
+    fetch('http://localhost:8000/api/recipes')
+      .then(r => r.json())
+      .then(data => setRecipes(data))
+  }, [])
+
+    
+
+    return (
+        <div>
+            <h1>レシピ一覧</h1>
+            <ul>
+                {recipes.map(recipe => (
+                    <li key={recipe.id}>
+                        {recipe.name} {recipe.cooked_count}回作りました。
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default RecipeList
